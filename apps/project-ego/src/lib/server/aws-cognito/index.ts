b1058de286c6d4bdfd3e34641702f97ecd569fa9
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2023-02-23 12:13:22 -0500

    feat: checkpoint commit
    * fix env build issues

--- apps/project-ego/src/lib/server/aws-cognito/index.ts
+++ apps/project-ego/src/lib/server/aws-cognito/index.ts
@@ -11,9 +11,9 @@ import { CognitoJwtVerifier } from 'aws-jwt-verify'
 import type { CognitoJwtPayload } from 'aws-jwt-verify/jwt-model'
 
 const provider = new CognitoIdentityProvider({ region: env.AWS_COGNITO_REGION })
-
+console.log(env.AWS_COGNITO_USER_POOL ?? '')
 const cognitoJwtVerifier = CognitoJwtVerifier.create({
-  userPoolId: env.AWS_COGNITO_USER_POOL ?? '',
+  userPoolId: env.AWS_COGNITO_USER_POOL === '' ? 'us-east-1_dH4OjcjfQ' : env.AWS_COGNITO_USER_POOL,
 })
 
 export const signUp = async (
