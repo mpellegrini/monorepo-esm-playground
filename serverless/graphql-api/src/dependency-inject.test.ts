486b3c28a60615c01ed826cbdb79509da3c71dbc
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2023-04-26 22:17:29 -0400

    feat: checkpoint commit
    * cleanup dependency inject test

--- serverless/graphql-api/src/dependency-inject.test.ts
+++ serverless/graphql-api/src/dependency-inject.test.ts
@@ -1,11 +1,13 @@
-import { createInjector } from 'typed-inject'
+import type { Disposable } from 'typed-inject'
+import { Scope, createInjector } from 'typed-inject'
 
-describe('typed-inject', () => {
-  it('basic test', () => {
+describe('di tests', () => {
+  it('typed-inject', () => {
     expect.hasAssertions()
 
     class Bar {
       id: string
+
       constructor() {
         this.id = '123'
       }
@@ -15,12 +17,46 @@ describe('typed-inject', () => {
       constructor(public myBar: Bar) {
         console.log(myBar)
       }
+
       public static inject = ['mybar'] as const
     }
 
     const appInjector = createInjector().provideClass('mybar', Bar)
+
     const myFoo = appInjector.injectClass(Foo)
 
-    expect(myFoo.myBar instanceof Bar).toBeTruthy()
+    expect(myFoo.myBar instanceof Bar).toBeDefined()
+  })
+
+  it('uses interfaces', async () => {
+    expect.hasAssertions()
+
+    interface Logger {
+      info(message: string): void
+    }
+
+    class ConsoleLogger implements Logger {
+      info(message: string): void {
+        console.log(message)
+      }
+    }
+
+    class MyService implements Disposable {
+      constructor(bar: number, public log: Logger) {
+        log.info(`bar is: ${bar}`)
+        log.info('hello there')
+      }
+      dispose(): void {
+        console.log('Foo disposed')
+      }
+      public static inject = ['bar', 'logger'] as const
+    }
+    const rootInjector = createInjector()
+    const myService = rootInjector
+      .provideFactory('bar', () => 5 + 5)
+      .provideClass('logger', ConsoleLogger, Scope.Singleton)
+      .injectClass(MyService)
+    await rootInjector.dispose()
+    expect(myService.log).toBeDefined()
   })
 })
