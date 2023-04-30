c0b7eb373ae0461273294980b1e717586c5f26ee
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2023-04-30 18:41:16 -0400

    feat: checkpoint commit
    * add initial svelte admin app

--- apps/storefront/src/routes/+layout.svelte
+++ apps/storefront/src/routes/+layout.svelte
@@ -5,3 +5,26 @@
 <div class="container mx-auto min-h-full border-2 border-dashed border-amber-600">
   <slot />
 </div>
+
+<picture>
+  <source
+    srcset="https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=1500&amp;h=1500&amp;fm=avif&amp;f=center&amp;fit=fill&amp;q=80 1500w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=1000&amp;h=1000&amp;fm=avif&amp;f=center&amp;fit=fill&amp;q=80 1000w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=808&amp;h=808&amp;fm=avif&amp;f=center&amp;fit=fill&amp;q=80 808w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=616&amp;h=616&amp;fm=avif&amp;f=center&amp;fit=fill&amp;q=80 616w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=424&amp;h=424&amp;fm=avif&amp;f=center&amp;fit=fill&amp;q=80 424w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=320&amp;h=320&amp;fm=avif&amp;f=center&amp;fit=fill&amp;q=80 320w"
+    sizes="(min-width: 1024px) 33vw, 50vw"
+    type="image/avif"
+    width="1000"
+    height="1000" /><source
+    srcset="https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=1500&amp;h=1500&amp;fm=webp&amp;f=center&amp;fit=fill&amp;q=80 1500w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=1000&amp;h=1000&amp;fm=webp&amp;f=center&amp;fit=fill&amp;q=80 1000w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=808&amp;h=808&amp;fm=webp&amp;f=center&amp;fit=fill&amp;q=80 808w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=616&amp;h=616&amp;fm=webp&amp;f=center&amp;fit=fill&amp;q=80 616w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=424&amp;h=424&amp;fm=webp&amp;f=center&amp;fit=fill&amp;q=80 424w,https://images.ctfassets.net/hnk2vsx53n6l/3PC0P9Khm496ZuIVpLweeL/2ee218bb55296bc338abb222057d7893/rbhtqhtwu0gjolioaxsi.png?w=320&amp;h=320&amp;fm=webp&amp;f=center&amp;fit=fill&amp;q=80 320w"
+    sizes="(min-width: 1024px) 33vw, 50vw"
+    type="image/webp"
+    width="1000"
+    height="1000" />
+
+  <img
+    aria-hidden="true"
+    sizes="(min-width: 1024px) 33vw, 50vw"
+    alt=""
+    loading="lazy"
+    width="1000"
+    height="1000"
+    class="src-components-atoms-ContentfulImage-img-3Kdd" />
+</picture>
