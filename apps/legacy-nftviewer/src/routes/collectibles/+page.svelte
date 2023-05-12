f9a67dc10fa0b9956d8963a96e954e06ac5ac857
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2023-05-11 20:35:04 -0400

    feat: checkpoint commit
    * add schools to legacy-nftviewer

--- apps/legacy-nftviewer/src/routes/collectibles/+page.svelte
+++ apps/legacy-nftviewer/src/routes/collectibles/+page.svelte
@@ -1,6 +1,34 @@
 <script lang="ts">
   import type { PageServerData } from './$types'
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const schools = {
+    UCF: {
+      name: 'University of Central Florida',
+      site: 'Charge On',
+    },
+    Oklahoma: {
+      name: 'The University of Oklahoma',
+      site: 'Boomer Sooner',
+    },
+    Villanova: {
+      name: 'Villanova University',
+      site: 'Nova Nation',
+    },
+    Clemson: {
+      name: 'Clemson University',
+      site: 'The Paw',
+    },
+    Kentucky: {
+      name: 'University of Kentucky',
+      site: 'Blue Chips',
+    },
+    Kansas: {
+      name: 'The University of Kansas',
+      site: 'Rock Chalk',
+    },
+  }
+
   export let data: PageServerData
 
   const collectibles = data.collectibles
