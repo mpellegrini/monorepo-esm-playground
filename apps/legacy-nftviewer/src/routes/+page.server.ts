7ceeedecbef4b3d0550e70d7f4ff7810db376251
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2023-06-01 18:17:38 -0400

    chore: remove legacy-nft-viewer app
    moved to standalone repo

--- apps/legacy-nftviewer/src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types'
-
-import { getSites } from '$lib/database'
-
-export const load = (() => {
-  return {
-    sites: getSites(),
-  }
-}) satisfies PageServerLoad
